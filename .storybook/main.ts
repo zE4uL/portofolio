0eea15b710e6727468b4797cdf4e05f824340f28
zE4uL <zE4uL@users.noreply.github.com>
2026-04-16 23:08:46 +0530

    chore(storybook): exclude archive, fix path, drop onboarding addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,18 +3,18 @@ import type { StorybookConfig } from '@storybook/nextjs-vite';
 const config: StorybookConfig = {
   "stories": [
     "../components/**/*.mdx",
-    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "!../components/_archive/**"
   ],
   "addons": [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
+    "@storybook/addon-docs"
   ],
   "framework": "@storybook/nextjs-vite",
   "staticDirs": [
-    "..\\public"
+    "../public"
   ]
 };
 export default config;
